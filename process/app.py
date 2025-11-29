a5e982c223989f99357aca2c215595e8a06f2521
seohyeon <rlatjgus2013@naver.com>
2025-11-29 12:42:29 +0900

    폴더 이름을 process로 변경하고 menu.html 수정

--- process/app.py
+++ process/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
-import socket   # socket 모듈 추가
+import socket   # 내 컴퓨터의 이름을 가져오기 위해 모듈 추가
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # hostname 값 설정
+    # 코드 추가
     if app.debug:
         hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
     else:
         hostname = ' '
-    # index.html 템플릿에 computername 값 전달
+    # render_templates에 computername 인자를 포함
     return render_template('index.html', computername=hostname)
 
 # /menu 라우트
@@ -19,4 +19,4 @@ def menu():
     return render_template('menu.html') 
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)  # debug 모드라 코드가 수정될 때 자동 수정,if 조건이 작동함
